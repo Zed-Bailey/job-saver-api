823328a03f3585de34acd2c7edeed9da7e854812
Zoran <emailzed03@gmail.com>
2023-11-24 14:35:15 +1100

    Update route.ts

--- app/api/job/route.ts
+++ app/api/job/route.ts
@@ -13,16 +13,52 @@ type RequestData = {
   sheetId: string
 }
 
-export async function POST (request: NextRequest) {
-  const res: RequestData | null = await request.json()
-  .then((e) => e)
-  .catch(() => null);
 
-  
+export async function POST (request: NextRequest) {
+  const res : RequestData| null = await request.json().catch(() => null);
+
+  console.log(res);
   if(res == null) {
-    return NextResponse.json({was: null});  
+    return NextResponse.json({error: "json was null"}, {status: 400});  
   }
-  
+
+  try {
+    
+    await AddRow(res);
+
+  } catch(e: any) {
+    console.error(e.message);
+    return NextResponse.json({error: "Looks like you haven't shared the sheet with the job-saver email"}, {status: 400});
+  }
+
+
+  return NextResponse.json({message: "Saved Job"});
+}
+
+
+export async function GET (request: NextRequest) {
+  const url = new URL(request.url);
+  const base64Body: string = url.searchParams.get("d") ?? "";
+  const res: RequestData = JSON.parse(atob(base64Body));
+
+  console.log(res);
+  if(base64Body == null || base64Body == "" || res == null) {
+    return NextResponse.json({error: "json was null"}, {status: 400});  
+  }
+
+  try {
+    
+    await AddRow(res);
+
+  } catch(e: any) {
+    return NextResponse.json({error: "Looks like you haven't shared the sheet with the job-saver email"}, {status: 400});
+  }
+
+
+  return NextResponse.json({message: "Saved Job"});
+}
+
+async function AddRow(data: RequestData) {
   
   // create a new google auth jwt token
   let token = new JWT({
@@ -31,23 +67,16 @@ export async function POST (request: NextRequest) {
     scopes: GoogleSheetsConfig.SCOPES,
   });
 
-  let sheet = new GoogleSpreadsheet(res.sheetId, token);
+  let sheet = new GoogleSpreadsheet(data.sheetId, token);
   
   let row: SheetRow = {
-    company: res.company,
-    role: res.role,
-    url: res.url
+    Company: data.company,
+    Role: data.role,
+    Url: data.url
   };
 
-  try {
-    await sheet.loadInfo();
-    await sheet.sheetsByIndex[0].addRow(row);
-  } catch(e: any) {
-    return NextResponse.json({error: "Looks like you haven't shared the sheet with the job-saver email"}, {status: 400});
-  }
-
   
-
-  return NextResponse.json({message: "Saved Job"});
-
+  await sheet.loadInfo();
+  await sheet.sheetsByIndex[0].addRow(row);
+    
 }
